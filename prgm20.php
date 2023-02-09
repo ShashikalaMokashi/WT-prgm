ee6f93de1885b03e3a304e75e6b76ece42443138
ShashikalaMokashi <116992756+ShashikalaMokashi@users.noreply.github.com>
2023-02-09 11:48:22 +0530

    Add files via upload

---
+++ prgm20.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Length of string</title>
+    </head>
+    <body>
+        <h1>write a php prg to find lenght of string  and count the word in the string </h1>
+        <form action="" method="POST">
+            <h1>Enter a string :  <input type="text" name="a"/></h1>
+            <input type="submit" name="submit1" value="check lenght"/>
+
+        </form>
+        <?php
+           if($_POST["submit1"])
+           {
+            $str1=$_POST["a"];
+            echo "<h2>";
+            echo "Given string is :" . $str1;
+            echo "<br> length of string:" . strlen($str1);
+            echo "<br>no of words in string:" . str_word_count($str1);
+            echo "</h2>";
+           }
+
+        ?>
+        
+    </body>
+    
+
+</html>
